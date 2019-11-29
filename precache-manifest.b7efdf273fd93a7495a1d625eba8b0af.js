03953c11d42db4876b7384565dbaa2e4428d8e9a
Philip Raschke <philip.raschke@tu-berlin.de>
2019-11-29 01:56:07 +0100

    initial commit

---
+++ precache-manifest.b7efdf273fd93a7495a1d625eba8b0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7e2db88db61609d987980c52c3f4d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffdd0680f094e1ab9dc7",
+    "url": "/static/css/main.c6b01dc8.chunk.css"
+  },
+  {
+    "revision": "f921efb633b67fae3a9a",
+    "url": "/static/js/2.6089c591.chunk.js"
+  },
+  {
+    "revision": "ffdd0680f094e1ab9dc7",
+    "url": "/static/js/main.bee88081.chunk.js"
+  },
+  {
+    "revision": "0e710a43f3d55fe550a3",
+    "url": "/static/js/runtime-main.a414502d.js"
+  },
+  {
+    "revision": "f1d24629d899725c1a34afbc28032efe",
+    "url": "/static/media/bg.f1d24629.png"
+  }
+]);
